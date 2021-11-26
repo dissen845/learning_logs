5d4851c32d63f736e6a4f855b6ec288a770df134
dissen845 <bleckmesa@mail.ru>
2021-11-26 10:31:52 +0300

    Добавлена настройка DEBUG

--- trening/settings.py
+++ trening/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,8 @@ LOGIN_URL = '/users/login/'
 #Настройки Heroku
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
